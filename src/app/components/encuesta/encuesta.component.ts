8792cc40dc180741ea33dd95b39c1652efc80d02
oljudi <oljudimark@gmail.com>
2018-11-23 13:43:21 -0600

    actualizacion Nav Bar resto de pestañas

--- src/app/components/encuesta/encuesta.component.ts
+++ src/app/components/encuesta/encuesta.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faFemale, faChartLine, faSignOutAlt, faSignInAlt, faHome, faVoteYea } from '@fortawesome/free-solid-svg-icons';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-encuesta',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EncuestaComponent implements OnInit {
 
-  constructor() { }
+  public isLogin: boolean;
+  public nombreUsuario: string;
+  public emailUsuario: string;
+
+  constructor(
+    public AuthService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.AuthService.getAuth().subscribe( auth =>{
+      if(auth){
+        this.isLogin=true;
+        this.nombreUsuario = auth.displayName;
+        this.emailUsuario = auth.email;
+      } else{
+        this.isLogin = false;
+      }
+    });
   }
 
+  onClickLogOut(){
+    this.AuthService.logout();
+  }
+
+  faFemale = faFemale;
+  faChartLine = faChartLine;
+  faSignOutAlt = faSignOutAlt;
+  faSignInAlt = faSignInAlt;
+  faHome = faHome;
+  faVoteYea = faVoteYea;
 }
