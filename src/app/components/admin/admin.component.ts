7dcc398fd8092c5976930cdd4b171c94f4ac4a72
jonathan huerta <mejahv_hs@hotmail.com>
2018-12-11 20:27:13 -0600

    array

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -16,39 +16,42 @@ import { faTintSlash } from '@fortawesome/free-solid-svg-icons';
 export class AdminComponent implements OnInit {
 
   listadoEncuestaex: any={};
-  list: Observable<EncuestaexInterface[]>;
-  p1: number[];
+  ens: any;
+  list: string[];
+  p1: EncuestaexInterface[]=[];
   sumas:number;
   sucursals:string;
   contador: number;
   contadorre: number;
-  invoiceCol: AngularFirestoreCollection<EncuestaexInterface>;
-  conten: Observable<EncuestaexInterface[]>;
-  invoiceArray: EncuestaexInterface[];
   constructor(
     private encuestaex: EncuestaService,
     private afs: AngularFirestore
   ) { 
     this.listadoEncuestaex = this.encuestaex.getAllEncuestaex();
-    
+    this.encuestaex.getAllEncuestaex().subscribe(id => this.list = id as Array<string>);
+   
   }
   cont(){
     this.afs.collection('Encuestaexes').valueChanges().subscribe(values => this.contadorre = values.length);
     this.afs.collection('type').valueChanges().subscribe(values => this.contador = values.length);
-    this.list = this.encuestaex.getOneEncuestaex();
+   
     
-
-    
+   // this.encuestaex.getAllEncuestaex().subscribe(id      => this.p1 = id );
+   // this.list = this.p1[0] as number;
+   // this.list = this.afs.collection('Encuestaexes').snapshotChanges().pipe(map(action => {return action.map( a => {const idz = a.payload.doc.id; return idz});}));
+   
+      
       this.afs.collection('Encuestaexes').doc('VI0001').valueChanges().pipe(take(1)).subscribe(res => {this.arras(res)} );
     
-    
-    //console.log(this.list);
+      //console.log(this.list);
     
 }
 
   arras( x: EncuestaexInterface){
     
       console.log(x.total);
+     // console.log(this.p1[0]);
+      console.log(this.list);
     
     
   }
