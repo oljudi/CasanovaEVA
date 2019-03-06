aeccf770c950e4e36e3a2b13be459ecd8c3df797
oljudi <oljudimark@gmail.com>
2019-03-06 13:32:36 -0600

    falta callcenter

--- src/app/components/dashboardtaller/dashboardtaller.component.ts
+++ src/app/components/dashboardtaller/dashboardtaller.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { faCarCrash, faSearch, faNotesMedical, faStickyNote, faPrint } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { faCarCrash, faSearch, faStickyNote, faPrint } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dashboardtaller',
   templateUrl: './dashboardtaller.component.html',
-  styleUrls: ['./dashboardtaller.component.css']
+  styleUrls: ['./dashboardtaller.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DashboardtallerComponent implements OnInit {
 
@@ -16,10 +17,31 @@ export class DashboardtallerComponent implements OnInit {
   faPrint = faPrint;
 
   // Variables
+  rows1: any[] = [];
+  expanded: any = {};
+  timeout: any;
+
+
+  @ViewChild('myTable') table: any;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onPage(event) {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      console.log('paged!', event);
+    }, 100);
+  }
+    toggleExpandRow(rows1) {
+    console.log('Toggled Expand Row!', rows1);
+    this.table.rowDetail.toggleExpandRow(rows1);
+  }
+
+  onDetailToggle(event) {
+    console.log('Detail Toggled', event);
   }
 
 }
