67dfb983f815e7c97dc76d5e1005190fc8826d91
oljudi <oljudimark@gmail.com>
2019-04-11 22:42:21 -0500

    Charts para muestra

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -14,46 +14,55 @@ export class ReportsComponent implements OnInit {
   // Pie
   public pieChartOptions: ChartOptions = {
     responsive: true,
-  }; 
+  };
   public pieChartType: ChartType = 'pie';
   public pieChartLegend = false;
   public pieChartPlugins = [];
   public pieChartLabels: Label[] = ['Bueno', ['Mas', 'ó', 'Menos'], 'Regular', 'Malo', 'Muy Malo'];
 
-    // P1
-    
-  public pieChartDataP1: SingleDataSet = [300, 200, 200, 100, 200];
+    // P1 El tiempo que trasncurrio para que lo atendieran fue...
 
-    //P2
-  public pieChartDataP2: SingleDataSet = [500, 100, 100, 100, 200];
+  public pieChartDataP1: SingleDataSet = [9, 6, 6, 3, 6];
 
-    //P3
-  public pieChartDataP3: SingleDataSet = [500, 100, 100, 100, 200];
+    // P2 ¿Como calificaría la imagen de nuestras instalaciones?
+  public pieChartDataP2: SingleDataSet = [15, 3, 3, 3, 6];
 
-    //P4
-  public pieChartDataP4: SingleDataSet = [500, 100, 100, 100, 200];
+    // P3 La atención que recibió de nuestro asesor de servicio fue….
+  public pieChartDataP3: SingleDataSet = [18, 6, 1.5, 1.5, 3];
 
-    //P5
-  public pieChartDataP5: SingleDataSet = [500, 100, 100, 100, 200];
+    // P4 Considera que la imagen de nuestro asesor de servicio es…
+  public pieChartDataP4: SingleDataSet = [21, 6, 1.5, .75, .75];
 
-    //P2
-  public pieChartDataP6: SingleDataSet = [500, 100, 100, 100, 200];
+    // P5 Le entregaron su vehículo …
+  public pieChartLabels5: Label[] = ['Limpio', 'Sucio'];
+  public pieChartDataP5: SingleDataSet = [21, 9];
 
-  // Bar 
-  // para preguntas por servicio si/no
-  public barChartOptions: ChartOptions = {
-    responsive: true,
-  };
-  public barChartLabels: Label[] = ['Enero', 'Febrero', 'Marzo', 'Abril'];
-  public barChartType: ChartType = 'bar';
-  public barChartLegend = true;
-  public barChartPlugins = [];
+    // P2 La experiencia en general de su visita a Casanova fue…
+  public pieChartDataP6: SingleDataSet = [12, 9, 6, 1.5, 1.5];
 
-  public barChartData: ChartDataSets[] = [
-    { data: [100, 70, 50, 0], label: 'SI' },
-    { data: [50, 40, 45, 0], label: 'NO' }
-  ];
-
+  // Bar
+  // para preguntas por servicio
+  // Express
+    // P1_E Informacion de servicio adicional...
+    public pieChartLabelP1_E: Label[] = ['SI', 'No'];
+    public pieChartDataP1_E: SingleDataSet = [21, 9];
+    // P2_E Tiempo de entrega...
+    public pieChartLabelP2_E: Label[] = ['SI', 'No'];
+    public pieChartDataP2_E: SingleDataSet = [6, 24];
+  // Tramite
+    // P1_T Informacion de servicio adicional...
+    public pieChartLabelP1_T: Label[] = ['SI', 'No'];
+    public pieChartDataP1_T: SingleDataSet = [12, 18];
+    // P2_T Entrega en tiempo...
+    public pieChartLabelP2_T: Label[] = ['SI', 'No'];
+    public pieChartDataP2_T: SingleDataSet = [30, 0];
+  // Reparacion
+    // P1_R Informacion de servicio adicional...
+    public pieChartLabelP1_R: Label[] = ['SI', 'No'];
+    public pieChartDataP1_R: SingleDataSet = [30, 0];
+    // P2_T Entrega en tiempo...
+    public pieChartLabelP2_R: Label[] = ['SI', 'No'];
+    public pieChartDataP2_R: SingleDataSet = [12, 18];
 
   /* Iconos */
   faChartLine = faChartLine;
@@ -72,62 +81,62 @@ export class ReportsComponent implements OnInit {
     monkeyPatchChartJsLegend();
    }
 
-   cp1mb:number;
-   cp1b:number;
-   cp1r:number;
-   cp1m:number;
-   cp1mm:number;
+   cp1mb: number;
+   cp1b: number;
+   cp1r: number;
+   cp1m: number;
+   cp1mm: number;
 
-   cp2mb:number;
-   cp2b:number;
-   cp2r:number;
-   cp2m:number;
-   cp2mm:number;
+   cp2mb: number;
+   cp2b: number;
+   cp2r: number;
+   cp2m: number;
+   cp2mm: number;
 
-   cp3mb:number;
-   cp3b:number;
-   cp3r:number;
-   cp3m:number;
-   cp3mm:number;
+   cp3mb: number;
+   cp3b: number;
+   cp3r: number;
+   cp3m: number;
+   cp3mm: number;
 
-   cp4mb:number;
-   cp4b:number;
-   cp4r:number;
-   cp4m:number;
-   cp4mm:number;
+   cp4mb: number;
+   cp4b: number;
+   cp4r: number;
+   cp4m: number;
+   cp4mm: number;
 
-   cp5mb:number;
-   cp5b:number;
-   cp5r:number;
-   cp5m:number;
-   cp5mm:number;
+   cp5mb: number;
+   cp5b: number;
+   cp5r: number;
+   cp5m: number;
+   cp5mm: number;
 
-   cp6mb:number;
-   cp6b:number;
-   cp6r:number;
-   cp6m:number;
-   cp6mm:number;
+   cp6mb: number;
+   cp6b: number;
+   cp6r: number;
+   cp6m: number;
+   cp6mm: number;
 
-   cp7mb:number;
-   cp7b:number;
-   cp7r:number;
-   cp7m:number;
-   cp7mm:number;
+   cp7mb: number;
+   cp7b: number;
+   cp7r: number;
+   cp7m: number;
+   cp7mm: number;
 
-   cp8mb:number;
-   cp8b:number;
-   cp8r:number;
-   cp8m:number;
-   cp8mm:number;
+   cp8mb: number;
+   cp8b: number;
+   cp8r: number;
+   cp8m: number;
+   cp8mm: number;
 
-   cp9mb:number;
-   cp9mm:number;
-   
-   cp10mb:number;
-   cp10mm:number;
-   
+   cp9mb: number;
+   cp9mm: number;
 
-   
+   cp10mb: number;
+   cp10mm: number;
+
+
+
   ngOnInit() {
     this.getData1();
     this.getData2();
@@ -138,7 +147,7 @@ export class ReportsComponent implements OnInit {
     this.afs.collection('Contadores/Pregunta1/Regular').valueChanges().subscribe(values => (this.cp1r = values.length) as number);
     this.afs.collection('Contadores/Pregunta1/Malo').valueChanges().subscribe(values => (this.cp1m = values.length) as number);
     this.afs.collection('Contadores/Pregunta1/MuyMalo').valueChanges().subscribe(values => (this.cp1mm = values.length) as number);
-    
+
     this.afs.collection('Contadores/Pregunta2/MuyBueno').valueChanges().subscribe(values => (this.cp2mb = values.length) as number);
     this.afs.collection('Contadores/Pregunta2/Bueno').valueChanges().subscribe(values => (this.cp2b = values.length) as number);
     this.afs.collection('Contadores/Pregunta2/Regular').valueChanges().subscribe(values => (this.cp2r = values.length) as number);
@@ -162,31 +171,31 @@ export class ReportsComponent implements OnInit {
     this.afs.collection('Contadores/Pregunta5/Regular').valueChanges().subscribe(values => (this.cp5r = values.length) as number);
     this.afs.collection('Contadores/Pregunta5/Malo').valueChanges().subscribe(values => (this.cp5m = values.length) as number);
     this.afs.collection('Contadores/Pregunta5/MuyMalo').valueChanges().subscribe(values => (this.cp5mm = values.length) as number);
-    
+
     this.afs.collection('Contadores/Pregunta6/MuyBueno').valueChanges().subscribe(values => (this.cp6mb = values.length) as number);
     this.afs.collection('Contadores/Pregunta6/Bueno').valueChanges().subscribe(values => (this.cp6b = values.length) as number);
     this.afs.collection('Contadores/Pregunta6/Regular').valueChanges().subscribe(values => (this.cp6r = values.length) as number);
     this.afs.collection('Contadores/Pregunta6/Malo').valueChanges().subscribe(values => (this.cp6m = values.length) as number);
     this.afs.collection('Contadores/Pregunta6/MuyMalo').valueChanges().subscribe(values => (this.cp6mm = values.length) as number);
-    
+
     this.afs.collection('Contadores/Pregunta7/MuyBueno').valueChanges().subscribe(values => (this.cp7mb = values.length) as number);
     this.afs.collection('Contadores/Pregunta7/Bueno').valueChanges().subscribe(values => (this.cp7b = values.length) as number);
     this.afs.collection('Contadores/Pregunta7/Regular').valueChanges().subscribe(values => (this.cp7r = values.length) as number);
     this.afs.collection('Contadores/Pregunta7/Malo').valueChanges().subscribe(values => (this.cp7m = values.length) as number);
     this.afs.collection('Contadores/Pregunta7/MuyMalo').valueChanges().subscribe(values => (this.cp7mm = values.length) as number);
-    
+
     this.afs.collection('Contadores/Pregunta8/MuyBueno').valueChanges().subscribe(values => (this.cp8mb = values.length) as number);
     this.afs.collection('Contadores/Pregunta8/Bueno').valueChanges().subscribe(values => (this.cp8b = values.length) as number);
     this.afs.collection('Contadores/Pregunta8/Regular').valueChanges().subscribe(values => (this.cp8r = values.length) as number);
     this.afs.collection('Contadores/Pregunta8/Malo').valueChanges().subscribe(values => (this.cp8m = values.length) as number);
     this.afs.collection('Contadores/Pregunta8/MuyMalo').valueChanges().subscribe(values => (this.cp8mm = values.length) as number);
-    
+
     this.afs.collection('Contadores/Pregunta9/Si').valueChanges().subscribe(values => (this.cp9mb = values.length) as number);
     this.afs.collection('Contadores/Pregunta9/No').valueChanges().subscribe(values => (this.cp9mm = values.length) as number);
-    
+
     this.afs.collection('Contadores/Pregunta10/Si').valueChanges().subscribe(values => (this.cp10mb = values.length) as number);
     this.afs.collection('Contadores/Pregunta10/No').valueChanges().subscribe(values => (this.cp10mm = values.length) as number);
-    
+
   }
 
   getData1() {
